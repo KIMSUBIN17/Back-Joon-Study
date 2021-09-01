da971d6dae6703571e1725197b720cb441ed6188
KIMSUBIN17 <tnqls24860@naver.com>
2021-09-01 16:04:51 +0900

    Update 11279 최대 힙.py

--- 11279 최대 힙.py
+++ 11279 최대 힙.py
@@ -16,3 +16,4 @@ for i in range(int(sys.stdin.readline().rstrip())):
 #1. 배열에 자연수 넣음
 #2. 배열에서 가장 큰 값 출력하고, 그 값을 배열에서 제거
 # 입력에서 0이 주어진 횟수만큼 답 출력. 배열이 비어있다면 0출력
+# 입력속도 향상위해 sys.stdin.readline()함수 이용
