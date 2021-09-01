d135b1d1ddcdd255ad454fc5e1b5c7f6862920de
KIMSUBIN17 <tnqls24860@naver.com>
2021-09-01 16:14:35 +0900

    Update 11279 최대 힙.py

--- 11279 최대 힙.py
+++ 11279 최대 힙.py
@@ -12,7 +12,8 @@ for i in range(int(sys.stdin.readline().rstrip())):
         heapq.heappush(heap,n)
         
 
-#최대힙 
+#파이썬의 heapq라이브러리 사용
+# heapq가 최소힙만 지원하므로 입력값을 음수로 처리해 최대힙처럼 사용
 #1. 배열에 자연수 넣음
 #2. 배열에서 가장 큰 값 출력하고, 그 값을 배열에서 제거
 # 입력에서 0이 주어진 횟수만큼 답 출력. 배열이 비어있다면 0출력
