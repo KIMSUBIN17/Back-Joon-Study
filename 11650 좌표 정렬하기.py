ac419efeca5e5a3cf9ce50ee9297439b8b57e73e
KIMSUBIN17 <tnqls24860@naver.com>
2021-09-19 00:45:59 +0900

    Update 11650 좌표 정렬하기.py

--- 11650 좌표 정렬하기.py
+++ 11650 좌표 정렬하기.py
@@ -1,3 +1,5 @@
+/*링크 : https://www.acmicpc.net/problem/11650*/
+
 n = int(input())
 
 arr=[]
@@ -10,7 +12,9 @@ arr = sorted(arr)
 for i in range(n):
     printarr[i][0], arr[i][1])
 
-#n 입력하고, n만큼 좌표입력
-#좌표입력받으면 정렬 수행
-#x좌표가 같으면 y좌표가 증가하는 순서대로 정렬 후 출력
-#입력받은 수를 list로 만들고 sorted로 정렬 후 출력
+'''
+1.n 입력하고, n만큼 좌표입력
+2.좌표입력받으면 정렬 수행
+3.x좌표가 같으면 y좌표가 증가하는 순서대로 정렬 후 출력
+4.입력받은 수를 list로 만들고 sorted로 정렬 후 출력
+'''
